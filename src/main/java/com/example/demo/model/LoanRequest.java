00ec98b6d2ffc601d9cf5d02ab40b356ae0333f8
bala-amypo <bala@amypo.in>
2025-12-20 15:50:57 +0000

    User commit

--- src/main/java/com/example/demo/model/LoanRequest.java
+++ src/main/java/com/example/demo/model/LoanRequest.java
@@ -1,4 +1,23 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.
+import lombok.Getter;
+import lombok.Setter;
+
+import java.swl.Timestamp;
+
+@Entity
+@Getter
+@Setter
+public class LoanRequest{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+    private Double requestedAmount;
+    private Integer tenureMonths;
+    private String purpose;
+    private String status="PENDI"
+}
