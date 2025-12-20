fb13b155f9e831b641da11dc2925aefd528c4bb8
bala-amypo <bala@amypo.in>
2025-12-20 15:37:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/RiskLogController.java
+++ src/main/java/com/example/demo/controller/RiskLogController.java
@@ -9,5 +9,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/risk-logs")
 public class RiskLogController{
-    @
+    @Autowired
+    RiskAssessmentLogService service;
+
+    @GetMapping("/{LoanRequestId}")
+    public List<RiskAssessmentLog>getlogs(@PathVariable Long loanRequestId){
+        return service.getLogsByRequest(loanRequestID);
+    }
 }
