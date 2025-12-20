84e9e0e3ec03e1abbdc9c692d5d63598789a4cac
bala-amypo <bala@amypo.in>
2025-12-20 16:35:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/LoanRequestServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.demo.service.impl;
+import com.example.demo.model.LoanRequest;
+import com.example.demo.repository.LoanRequestRepository;
+import com.example.service.LoanRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.sterotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoanRequestServiceImpl
+implements LoanRequestService{
+    
+}
