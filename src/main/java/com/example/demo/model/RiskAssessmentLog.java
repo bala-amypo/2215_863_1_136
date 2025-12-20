e289628473402b5d6418809ab74945c87938fe49
bala-amypo <bala@amypo.in>
2025-12-20 15:59:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/RiskAssessmentLog.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+public class RiskAssessmentLog{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long loanRequestedId;
+    private Double dtiRatio;
+    private String creditCheckStatus;
+
+    private Timestamp timestamp = new Timestamp
+}
