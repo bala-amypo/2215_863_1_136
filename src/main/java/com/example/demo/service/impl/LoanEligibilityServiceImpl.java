4c95c284a3a35d7497e8ca24e98db5c2ddb60966
bala-amypo <bala@amypo.in>
2025-12-20 16:44:57 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LoanEligibilityServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoanEligibilityServiceImpl.java
@@ -1,4 +1,16 @@
 package com.example.demo.service.impl;
 import com.example.demo.model.*;
 import com.example.demo.repository.*;
-import com.example.demo
+import com.example.demo.service.LoanEligibilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.sterotype.Service;
+
+@Service
+public class LoanEligibilityServiceImpl implements LoanEligibilityService{
+    @Autowired LoanRequestRepository loanRepo;
+    @Autowired FinancialProfileRepository profileRepo;
+    @Autowired EligibiltyResultRepository resultRepo;
+    @Autowired RiskAssessmentLogRepository logpo;
+
+    public Eligibil
+}
