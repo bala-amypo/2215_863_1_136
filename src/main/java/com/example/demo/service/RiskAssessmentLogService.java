f8ca567c774268a83ac97226cd929e85537d7681
bala-amypo <bala@amypo.in>
2025-12-20 16:23:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/RiskAssessmentLogService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+import com.example.demo.model.RiskAssessmentLog;
+import java.util.List;
+
+public interface RiskAssessmentLogService{
+    List<RiskAssessmentLog>getLogsByRequest(Long loanRequestId);
+}
