30e8a30fc1c3e612212af3a4d261d02c065a9612
bala-amypo <bala@amypo.in>
2025-12-20 16:59:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskAssessmentLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskAssessmentLogServiceImpl.java
@@ -3,3 +3,16 @@ import com.example.demo.model.RiskAssessmentLog;
 import com.example.demorepository.RislAssessmentLogRespository;
 import com.example.demo.service.RiskAssessmentLogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.sterotype.Service;
+
+import java.util.List;
+
+@Service
+public class RiskAssessmentLogServiceImpl implements RiskAssessmentLogService{
+    @Autowired
+    RiskAssessmentLogRepository repo;
+
+    public List<RiskAssessmentLog>getLogsByRequest(Long loanRequestID){
+        return repo.findByRequestd(loanRequestId);
+    }
+}
