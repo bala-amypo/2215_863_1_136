fa98c82ae663ea5f8dbc2995127fe611526b19ab
bala-amypo <bala@amypo.in>
2025-12-20 16:56:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/RiskAssessmentLogServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.demo.service.impl;
+import com.example.demo.model.RiskAssessmentLog;
+import com.example.demorepository.RislAssessmentLogRespository;
+import com.example.demo.service.RiskAssessmentLogService;
+import org.springframework.beans.factory.annotation.Autowired;
