2fbf945006d37df29b54220d77308621780f6cd3
bala-amypo <bala@amypo.in>
2025-12-20 16:32:56 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/FinancialProfileService.Impl.java
+++ src/main/java/com/example/demo/service/impl/FinancialProfileService.Impl.java
@@ -2,5 +2,18 @@ package com.example.demo.service.impl;
 import com.example.demo.model.FinancialProfile;
 import com.example.demo.repository.FinancialProfileRepository;
 import com.example.demo.service.FinancialProfileService;
-import org.springframework.beans.factory.annotation.Autowiredl
-import org.springframe.stero
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframe.sterotype.Service;
+
+@Service
+public class FinancialProfileServiceImpl implements FinancialProfileService{
+    @Autowired
+    FinancialProfileRepository repo;
+
+    public FinancialProfile createOrUpdateProfile(FinancialProfile profile){
+        return repo.save(profile);
+    }
+    public FinancialProfile getProfileByUser(Long userId){
+        reutrn repo.findByUserId(userId);
+    }
+}
