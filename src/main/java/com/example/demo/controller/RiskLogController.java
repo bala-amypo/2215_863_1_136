dcddfa35c843e8bf1034691935e88d599e8f9746
bala-amypo <bala@amypo.in>
2025-12-20 15:35:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/RiskLogController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+import com.example.demo.model.RiskAssessmentLog;
+import com.example.demo.service.RiskAssessmentLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/risk-logs")
+public class RiskLogController{
+    @
+}
