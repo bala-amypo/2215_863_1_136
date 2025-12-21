2ff953762b369fcc4c8119e0deafb4b42992df92
bala-amypo <bala@amypo.in>
2025-12-21 16:40:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/LoanRequest.java
@@ -0,0 +1,32 @@
+package com.example.demo.dto;
+
+public class LoanRequestDto {
+
+    private Double amount;
+    private Integer tenureMonths;
+    private String purpose;
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Integer getTenureMonths() {
+        return tenureMonths;
+    }
+
+    public void setTenureMonths(Integer tenureMonths) {
+        this.tenureMonths = tenureMonths;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+}
