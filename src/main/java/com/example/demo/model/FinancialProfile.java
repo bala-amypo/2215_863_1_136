23a61148230a11064f3c0edd91aab3617173ac58
bala-amypo <bala@amypo.in>
2025-12-23 05:18:33 +0000

    User commit

--- src/main/java/com/example/demo/model/FinancialProfile.java
+++ src/main/java/com/example/demo/model/FinancialProfile.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 import java.sql.Timestamp;
+import  com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
@@ -13,6 +14,7 @@ public class FinancialProfile {
     private Long id;
 
     @OneToOne
+    @JsonIgnore
     private User user;
 
     private Double monthlyIncome;
