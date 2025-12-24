0286004cf70d0ba02fd0f6cfe47701f157e5fde7
bala-amypo <bala@amypo.in>
2025-12-24 04:37:31 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskAssessmentLogService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.RiskAssessmentLog;
-import java.util.List;
-
-public interface RiskAssessmentLogService {
-    void logAssessment(RiskAssessmentLog log);
-    List<RiskAssessmentLog> getLogsByRequest(Long requestId);
-}
