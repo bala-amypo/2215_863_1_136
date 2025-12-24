4c08fdc34aaebf7e3ec62e42b508d9a09a3f9a17
bala-amypo <bala@amypo.in>
2025-12-24 04:01:31 +0000

    User commit

--- src/main/java/com/example/demo/service/FinancialProfileService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.FinancialProfile;
-import com.example.demo.model.User;
-
-public interface FinancialProfileService {
-    FinancialProfile createOrUpdateProfile(FinancialProfile profile);
-    FinancialProfile getProfileByUser(User user);
-}
