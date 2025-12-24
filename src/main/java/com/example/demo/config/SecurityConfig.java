ac7b71c40c9bbe54b13f9f5731ccfe5891b42085
bala-amypo <bala@amypo.in>
2025-12-24 05:07:31 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import com.example.demo.security.JwtFilter;
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,6 +29,12 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(auth -> auth
+
+                // ✅ Allow internal forwards & errors
+                .dispatcherTypeMatchers(
+                    DispatcherType.ERROR,
+                    DispatcherType.FORWARD
+                ).permitAll()
 
                 // ✅ Swagger URLs
                 .requestMatchers(
